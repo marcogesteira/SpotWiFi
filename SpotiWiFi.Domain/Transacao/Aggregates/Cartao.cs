031263b69cf2fb4da2c3106436fd8a6551b5fd05
Marco Gesteira <mgesteira@outlook.com>
2023-12-14 18:18:57 -0300

    Feat adicionadas regras de validação do negócio

--- SpotiWiFi.Domain/Transacao/Aggregates/Cartao.cs
+++ SpotiWiFi.Domain/Transacao/Aggregates/Cartao.cs
@@ -1,4 +1,5 @@
 ﻿using SpotiWiFi.Domain.Core.ValueObject;
+using SpotiWiFi.Domain.Transacao.ValueObject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,67 @@ namespace SpotiWiFi.Domain.Transacao.Aggregates
 {
     public class Cartao
     {
+        private const int INTERVALO_TRANSACAO = -2;
+        private const int REPETICAO_TRANSACAO_MERCHANT = 1;
+
         public Guid Id { get; set; }
         public Boolean Ativo { get; set; }
         public Monetario Limite { get; set; }
         public String Numero { get; set; }
         public List<Transacao> Transacoes { get; set; } = new List<Transacao>();
+
+        public void CriarTransacao(Merchant merchant, Monetario valor, string descricao = "")
+        {
+            //Verificar se o cartão está ativo
+            this.IsCartaoAtivo();
+
+            Transacao transacao = new Transacao();
+            transacao.Merchant = merchant;
+            transacao.Valor = valor;
+            transacao.Descricao = descricao;
+            transacao.DtTransacao = DateTime.Now;
+
+            //Verifica limite disponível
+            this.VerificaLimite(transacao);
+
+            //Verifica regras antifraude
+            this.ValidarTransacao(transacao);
+        }
+
+        private void ValidarTransacao(Transacao transacao)
+        {
+            var ultimasTransacoes = this.Transacoes.Where(x =>
+                                                          x.DtTransacao >= DateTime.Now.AddMinutes(INTERVALO_TRANSACAO));
+            if (ultimasTransacoes?.Count() >= 3)
+            {
+                throw new Exception("Cartão utilizado muitas vezes em um curto período de tempo");
+            }
+
+            var transacaoRepetidaPorMerchant = ultimasTransacoes?
+                                                .Where(x => x.Merchant.Nome.ToUpper() == transacao.Merchant.Nome.ToUpper()
+                                                       && x.Valor == transacao.Valor)
+                                                .Count() == REPETICAO_TRANSACAO_MERCHANT;
+
+            if (transacaoRepetidaPorMerchant)
+            {
+                throw new Exception("Transação duplicada para o mesmo cartão e o mesmo Comerciante");
+            }
+        }
+
+        private void VerificaLimite(Transacao transacao)
+        {
+            if (this.Limite < transacao.Valor)
+            {
+                throw new Exception("Cartão não possui limite para esta transação");
+            }
+        }
+
+        private void IsCartaoAtivo()
+        {
+            if (this.Ativo == false)
+            {
+                throw new Exception("Cartão não está ativo");
+            }
+        }
     }
 }
