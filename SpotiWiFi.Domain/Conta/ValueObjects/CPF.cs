c8ec4bfd02f0600453bcdb2e0b261e7d906592c5
Marco Gesteira <mgesteira@outlook.com>
2023-12-16 00:07:21 -0300

    feat Adicionada regra de validação do CPF e teste simples do CPF

--- SpotiWiFi.Domain/Conta/ValueObjects/CPF.cs
+++ SpotiWiFi.Domain/Conta/ValueObjects/CPF.cs
@@ -9,21 +9,72 @@ namespace SpotiWiFi.Domain.Conta.ValueObjects
 {
     public record CPF
     {
-        public static implicit operator int(CPF d) => d.Valor;
-        public static implicit operator CPF(int valor) => new CPF(valor);
+        public String Numero { get; set; }
 
-        public int Valor { get; set; }
-
-        public CPF(int valor)
+        public CPF(string numero)
         {
-            if (valor <= 0)
-                throw new ArgumentException("Insira um CPF Válido");
+            if (this.ValidaCPF(numero))
+                throw new Exception("CPF Inválido");
 
-            this.Valor = valor;
+            Numero = numero;
         }
-        public String Formatado()
+
+        private bool ValidaCPF(string numero)
         {
-            return $"{this.Valor.ToString()}";
+            string[] numeros = new string[9];
+            string[] digitos = new string[2];
+
+            //Recortar números do CPF
+            for (int i = 0; i < 9; i++)
+            {
+                numeros[i] = numero.Substring(i, 1);
+            }
+
+            //Recortar dígitos verificadores
+            for (int i = 0; i < 2; i++)
+            {
+                digitos[i] = numero.Substring((i + 9), 1);
+            }
+
+            //Somatório dos 9 números
+            int soma = 0;
+            int fator = 10;
+            for (int i = 0; i < 9; i++)
+            {
+                soma += (fator * int.Parse(numeros[i]));
+                fator--;
+            }
+            //Cálculo do 1º dígito verificador
+            int resto = soma % 11;
+            int primeiroDigito = 11 - resto;
+            if (primeiroDigito >= 10)
+            {
+                primeiroDigito = 0;
+            }
+
+            //Somatório dos 10 números
+            soma = 0;
+            fator = 11;
+            for (int i = 0; i < 9; i++)
+            {
+                soma += (fator * int.Parse(numeros[i]));
+                fator--;
+            }
+            soma += primeiroDigito * 2;
+            //Cálculo do 2º dígito verificador
+            resto = soma % 11;
+            int segundoDigito = 11 - resto;
+            if (segundoDigito >= 10)
+            {
+                segundoDigito = 0;
+            }
+
+            //Validação dos dígitos
+            if (primeiroDigito == int.Parse(digitos[0]) && segundoDigito == int.Parse(digitos[1]))
+            {
+                return false;
+            }
+            else { return true; }
         }
     }
 }
---
+++ SpotiWiFi.Test/Domain/CPFTests.cs
@@ -0,0 +1,34 @@
+﻿using SpotiWiFi.Domain.Conta.ValueObjects;
+using SpotiWiFi.Domain.Transacao.Aggregates;
+using SpotiWiFi.Domain.Transacao.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotiWiFi.Test.Domain
+{
+    public class CPFTests
+    {
+        [Fact]
+        public void CpfValido()
+        {
+            CPF cpf = new CPF("12137346737");
+            Assert.True(cpf.Numero == "12137346737");
+        }
+        //[Fact]
+        //public void PrimeiroDigitoInvalido()
+        //{
+        //    CPF cpf = new CPF("12137346747");
+        //    Assert.Throws<SystemException> () => cpf = new CPF("12137346747");
+        //}
+        //[Fact]
+        //public void SegundoDigitoInvalido()
+        //{
+        //    CPF cpf = new CPF("12137346738");
+        //    Assert.Throws<System.Exception>(
+        //        () => 'CPF Inválido');
+        //}
+    }
+}
