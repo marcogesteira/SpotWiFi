34a21b7d52f41c8f6fdd863afa1252409e0e7da6
Marco Gesteira <mgesteira@outlook.com>
2024-02-14 16:20:10 -0300

    feat: Adicionado o projeto de API

---
+++ SpotiWiFi.Api/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using SpotiWiFi.Repository;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SpotiWiFiContext>(c =>
+{
+    c.UseSqlServer(builder.Configuration.GetConnectionString("SpotiWiFiConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
