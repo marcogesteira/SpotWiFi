d50ff31c4238c29e89da14939f58b29f02dac288
Marco Gesteira <mgesteira@outlook.com>
2024-02-24 11:20:48 -0300

    Modificando UsuárioDto

--- SpotiWiFi.Application/Conta/Dto/UsuarioDto.cs
+++ SpotiWiFi.Application/Conta/Dto/UsuarioDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,22 @@ namespace SpotiWiFi.Application.Conta.Request
     public class UsuarioDto
     {
         public Guid Id { get; set; }
+        [Required]
         public String Nome { get; set; }
+        [Required]
+        [EmailAddress]
         public String Email { get; set; }
+        [Required]
         public String Senha { get; set; }
+        [Required]
         public DateTime DtNascimento { get; set; }
+        [Required]
         public String CPF { get; set; }
+        [Required]
         public EnderecoCobrancaDto EnderecoCobranca { get; set; }
 
         public Guid PlanoId { get; set; }
+        [Required]
         public CartaoDto Cartao { get; set; }
     }
 }
