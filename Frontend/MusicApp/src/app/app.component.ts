1fe6f1267ecee5d1b2503c348d32c3a7b5c825c7
Marco Gesteira <mgesteira@outlook.com>
2024-04-02 23:36:11 -0300

    Criação do HomeComponent do Angular

--- Frontend/MusicApp/src/app/app.component.ts
+++ Frontend/MusicApp/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'MusicApp';
+  title = 'SpotiWiFi';
 }
