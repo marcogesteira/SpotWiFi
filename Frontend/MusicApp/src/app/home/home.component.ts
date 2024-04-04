c2f259569ce4e6d045152646b6e187c177ef9d2a
Marco Gesteira <mgesteira@outlook.com>
2024-04-03 21:45:12 -0300

    Criando o Service e o Model de Banda

--- Frontend/MusicApp/src/app/home/home.component.ts
+++ Frontend/MusicApp/src/app/home/home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { BandaService } from '../services/banda.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, HttpClientModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit{
+
+  bandas = null;
+
+  constructor(private bandaService: BandaService) {
+  }
+  ngOnInit(): void {
+    this.bandaService.getBanda().subscribe(response => {
+      console.log(response);
+      this.bandas = response as any;
+    })
+  }
+}
