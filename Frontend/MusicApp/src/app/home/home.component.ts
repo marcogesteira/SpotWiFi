102f3f271b6325df11313f5eb05848ff55c37aef
Marco Gesteira <mgesteira@outlook.com>
2024-04-03 21:54:42 -0300

    Ajustes de layout

--- Frontend/MusicApp/src/app/home/home.component.ts
+++ Frontend/MusicApp/src/app/home/home.component.ts
@@ -4,24 +4,34 @@ import { MatCardModule } from '@angular/material/card';
 import { BandaService } from '../services/banda.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Router } from '@angular/router';
+import { Banda } from '../model/banda';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, HttpClientModule, CommonModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    CommonModule,
+    FlexLayoutModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
-
+export class HomeComponent implements OnInit {
   bandas = null;
 
-  constructor(private bandaService: BandaService) {
-  }
+  constructor(private bandaService: BandaService, private router: Router) {}
   ngOnInit(): void {
-    this.bandaService.getBanda().subscribe(response => {
+    this.bandaService.getBanda().subscribe((response) => {
       console.log(response);
       this.bandas = response as any;
-    })
+    });
+  }
+  public goToDetail(item: Banda) {
+    this.router.navigate(['detail', item.id]);
   }
 }
