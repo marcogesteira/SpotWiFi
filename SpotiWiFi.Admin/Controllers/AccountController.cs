93c13247fb8dce359b51b045877e348e062bac3a
Marco Gesteira <mgesteira@outlook.com>
2024-07-03 23:13:32 -0300

    Começando a criar a página de login do Admin

---
+++ SpotiWiFi.Admin/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpotiWiFi.Admin.Models;
+using SpotiWiFi.Application.Admin;
+
+namespace SpotiWiFi.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UsuarioAdminService _usuarioAdminService;
+
+        public AccountController(UsuarioAdminService usuarioAdminService)
+        {
+            _usuarioAdminService = usuarioAdminService;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
