3c1353bb0d00ca091fb81afa404b8f54b34868b7
Marco Gesteira <mgesteira@outlook.com>
2024-09-25 23:24:25 -0300

    Apontamento do STS para o BD novo.

--- SpotiWiFi.STS/Program.cs
+++ SpotiWiFi.STS/Program.cs
@@ -27,12 +27,9 @@ builder.Services.AddIdentityServer()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
